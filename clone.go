ae0603c51bdc477bb395b67191200ab794f700f8
Matthew Davis <matthew@matthewdavis.io>
2024-08-03 01:41:51 -0500

    bump

--- clone.go
+++ clone.go
@@ -1,50 +1,35 @@
 package main
 
 import (
-	"io"
-	"log"
-	"os"
-
 	git "github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/transport/http"
 )
 
 // ProgressLogger implements the io.Writer interface to log progress
 type ProgressLogger struct {
-	logger *log.Logger
+	ch chan string
 }
 
 func (pl *ProgressLogger) Write(p []byte) (n int, err error) {
-	pl.logger.Printf("%s", p)
+	pl.ch <- string(p)
 	return len(p), nil
 }
 
-func Clone(url string, token string, path string, branch string) (*git.Repository, error) {
-	// Create a log file
-	logFile, err := os.Create(path + "/clone.log")
-	if err != nil {
-		return nil, err
-	}
-	defer logFile.Close()
+func Clone(url string, token string, path string, branch string, ch chan string) (*git.Repository, error) {
+	progressLogger := &ProgressLogger{ch}
 
-	// Create a multi-writer to log to both file and stdout
-	multiWriter := io.MultiWriter(logFile, os.Stdout)
-	logger := log.New(multiWriter, "", log.LstdFlags)
-
-	progressLogger := &ProgressLogger{logger: logger}
-
-	// ... existing code ...
 	r, err := git.PlainClone(path, false, &git.CloneOptions{
 		Auth: &http.BasicAuth{
 			Username: "risersh",
 			Password: token,
 		},
-		URL:      url,
-		Progress: progressLogger,
+		URL:               url,
+		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+		Progress:          progressLogger,
 	})
 	if err != nil {
 		return nil, err
 	}
 
-	return r, err
+	return r, nil
 }
--- clone_test.go
+++ clone_test.go
@@ -1,13 +1,43 @@
 package main
 
 import (
+	"context"
+	"log"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestClone(t *testing.T) {
-	_, err := Clone("https://github.com/risersh/risersh", os.Getenv("GITHUB_TOKEN"), "/tmp/risersh/docker-build-example", "main")
+	repoPath := "/tmp/risersh/docker-build-example"
+
+	// Ensure cleanup after the test
+	defer func() {
+		err := os.RemoveAll(repoPath)
+		if err != nil {
+			t.Logf("Failed to clean up test directory: %v", err)
+		}
+	}()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	ch := make(chan string)
+
+	go func() {
+		for {
+			select {
+			case msg := <-ch:
+				log.Printf("%s", msg)
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	_, err := Clone("https://github.com/risersh/docker-build-example", os.Getenv("GITHUB_TOKEN"), repoPath, "main", ch)
 	if err != nil {
 		t.Fatal(err)
 	}
+
 }
